847bf550d12510a7a6ae2d439c246f603c0b5e5c
Evgeni Golovin <75369509+golovinapp@users.noreply.github.com>
2026-02-23 17:35:51 +0400

    update .env.example

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN_IDS = [int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", "5432"))
